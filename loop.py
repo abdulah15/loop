19890f993e0f0653993d1b2dd95b27744183662c
Abdullah <abdullahrehan2010@gmail.com>
2024-11-28 13:58:37 +0500

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,7 @@
+
+num = 23
+
+print(f"Table of {num}:")
+for i in range(1, 11):
+    result = num * i
+    print(f"{num} x {i} = {result}")
---
+++ star.py
@@ -0,0 +1,10 @@
+
+star = 5 
+
+print("Star Pattern = ")
+
+for i in range(1, star - 1): 
+    for j in range(i): 
+        
+        print("*", end="") 
+    print()  
